e2268a0ebddfbc38d2b98d820466e4b28bc6edc8
monalisinghi <Creative@18>
2020-10-06 18:38:08 +1100

    Added routing functionality for Dashboard page. Fixed typo in SurveyList component.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import SurveyList from "./components/SuveryList";
+import AppRouter from "./Routes";
 
 function App() {
   return (
@@ -22,8 +22,7 @@ function App() {
           </header>
         </Col>
       </Row>
-      <h1 className="mt-2 mb-4">Dashboard</h1>
-      <SurveyList />
+      <AppRouter />
     </Container>
   );
 }
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SurveyList from "../components/SurveyList";
+
+export default function Dashboard() {
+  return (
+    <>
+      <h1 className="mt-2 mb-4">Dashboard</h1>
+      <SurveyList />
+    </>
+  );
+}
