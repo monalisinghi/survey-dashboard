cb01ce470eff88ba61dad333aa47fe37af4e3c67
monalisinghi <Creative@18>
2020-10-06 18:25:01 +1100

    Added unit test for index.js and App.js.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import App from "./App";
+import { render } from "@testing-library/react";
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  it("should rendered H1 text content", () => {
+    const { getByText } = render(<App />);
+    expect(getByText("Dashboard")).toBeInTheDocument();
+  });
 });
